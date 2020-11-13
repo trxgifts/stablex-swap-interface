44071a15460d49e1091f2fe1e492288ce586550f
stableXO2 <oranges.master@outlook.com>
2020-11-13 22:44:30 +0800

    fix: updated

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,7 +4,7 @@ import { AbstractConnector } from '@web3-react/abstract-connector'
 // import { bsc, fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 import { injected, bsc, walletconnect } from '../connectors'
 
-export const ROUTER_ADDRESS = '0xC92A405C545b356066A5110FA5ab18AAdc89FA1f'
+export const ROUTER_ADDRESS = '0xc7fA7951187F91153851b0F9Ab2d032Abd0f2f0C'
 
 // a list of tokens by chain
 type ChainTokenList = {
