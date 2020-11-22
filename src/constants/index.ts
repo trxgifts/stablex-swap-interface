2a856259b57b6cec26ea5a8991fbba0d8b975e66
stablexswapdev <73826792+stablexswapdev@users.noreply.github.com>
2020-11-22 21:04:34 +0800

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -28,7 +28,7 @@ const WETH_ONLY: ChainTokenList = {
 // used to construct intermediary pairs for trading
 export const BASES_TO_CHECK_TRADES_AGAINST: ChainTokenList = {
   ...WETH_ONLY,
-  [ChainId.MAINNET]: [DAI, BUSD, USDT, USDC]
+  [ChainId.MAINNET]: [DAI, BUSD, USDT, USDC, QUSD]
 }
 
 /**
