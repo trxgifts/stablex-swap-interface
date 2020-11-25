b5c5b4a60537a23d6b31e4f413e88590d833d0fe
stablexswapdev <73826792+stablexswapdev@users.noreply.github.com>
2020-11-25 18:28:00 +0800

    Update ConfirmAddModalBottom.tsx

--- src/pages/AddLiquidity/ConfirmAddModalBottom.tsx
+++ src/pages/AddLiquidity/ConfirmAddModalBottom.tsx
@@ -58,7 +58,7 @@ export function ConfirmAddModalBottom({
         <TYPE.body>{noLiquidity ? '100' : poolTokenPercentage?.toSignificant(4)}%</TYPE.body>
       </RowBetween>
       <ButtonPrimary style={{ margin: '20px 0 0 0' }} onClick={onAdd}>
-        <Text fontWeight={500} fontSize={20} color={theme.colors.primary1} >
+        <Text fontWeight={500} fontSize={20} color=“#D3D3D3" >
           {noLiquidity ? 'Create Pool & Supply' : 'Confirm Supply'}
         </Text>
       </ButtonPrimary>
