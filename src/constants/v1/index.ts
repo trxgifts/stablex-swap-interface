45fa1c6746bf1d3ba02aca993d2c44f97abe5638
stablexswapdev <73826792+stablexswapdev@users.noreply.github.com>
2020-11-29 22:32:23 +0800

    Update index.ts

--- src/constants/v1/index.ts
+++ src/constants/v1/index.ts
@@ -4,8 +4,8 @@ import V1_EXCHANGE_ABI from './v1_exchange.json'
 import V1_FACTORY_ABI from './v1_factory.json'
 
 const V1_FACTORY_ADDRESSES: { [chainId in ChainId]: string } = {
-  [ChainId.MAINNET]: '0xef19CaF13168cfdf1ef539A1969edFC90a54FD0a', //TODO
-  [ChainId.BSCTESTNET]: '0xef19CaF13168cfdf1ef539A1969edFC90a54FD0a'
+  [ChainId.MAINNET]: '0xc5a00c937c19cfd33063a9a389c506a0df7d2fb3', 
+  [ChainId.BSCTESTNET]: '0x69F124636c7f56c52474f69A893035aDf9D29fE9'
 }
 
 const V1_FACTORY_INTERFACE = new Interface(V1_FACTORY_ABI)
