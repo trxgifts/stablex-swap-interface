95008c1123fca58c35ecb4133a69e499674772e2
stablexswapdev <73826792+stablexswapdev@users.noreply.github.com>
2020-12-02 10:29:57 +0800

    going back to og

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -20,40 +20,34 @@ type ChainTokenList = {
   readonly [chainId in ChainId]: Token[]
 }
 
-export const tDAI = new Token(
-  ChainId.BSCTESTNET,
-  '0xec5dcb5dbf4b114c9d0f65bccab49ec54f6a0867',
-  18,
-  'DAI',
-  'Dai Stablecoin'
-)
-export const tBUSD = new Token(
-  ChainId.BSCTESTNET,
-  '0xed24fc36d5ee211ea25a80239fb8c4cfd80f12ee',
-  18,
-  'BUSD',
-  'Binance USD'
-)
-export const tUSDT = new Token(
-  ChainId.BSCTESTNET,
-  '0x337610d27c682e347c9cd60bd4b3b107c9d34ddd',
-  18,
-  'USDT',
-  'Tether USD'
-)
-export const tUSDC = new Token(ChainId.BSCTESTNET, '0x64544969ed7ebf5f083679233325356ebe738930', 18, 'USDC', 'USD Coin')
+// export const tDAI = new Token(
+//   ChainId.BSCTESTNET,
+//   '0xec5dcb5dbf4b114c9d0f65bccab49ec54f6a0867',
+//   18,
+//   'DAI',
+//   'Dai Stablecoin'
+// )
+// export const tBUSD = new Token(
+//   ChainId.BSCTESTNET,
+//   '0xed24fc36d5ee211ea25a80239fb8c4cfd80f12ee',
+//   18,
+//   'BUSD',
+//   'Binance USD'
+// )
+// export const tUSDT = new Token(
+//   ChainId.BSCTESTNET,
+//   '0x337610d27c682e347c9cd60bd4b3b107c9d34ddd',
+//   18,
+//   'USDT',
+//   'Tether USD'
+// )
+// export const tUSDC = new Token(ChainId.BSCTESTNET, '0x64544969ed7ebf5f083679233325356ebe738930', 18, 'USDC', 'USD Coin')
 
 export const DAI = new Token(ChainId.MAINNET, '0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3', 18, 'DAI', 'Dai Stablecoin')
 export const BUSD = new Token(ChainId.MAINNET, '0xe9e7cea3dedca5984780bafc599bd69add087d56', 18, 'BUSD', 'Binance USD')
 export const USDT = new Token(ChainId.MAINNET, '0x55d398326f99059ff775485246999027b3197955', 18, 'USDT', 'Tether USD')
 export const USDC = new Token(ChainId.MAINNET, '0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d', 18, 'USDC', 'USD Coin')
-export const QUSD = new Token(
-  ChainId.MAINNET,
-  '0xb8c540d00dd0bf76ea12e4b4b95efc90804f924e',
-  18,
-  'QUSD',
-  'Qian Stablecoin'
-)
+export const QUSD = new Token(ChainId.MAINNET, '0xb8c540d00dd0bf76ea12e4b4b95efc90804f924e', 18, 'QUSD','Qian Stablecoin')
 
 // export const EOS = new Token(ChainId.MAINNET, '0x56b6fb708fc5732dec1afc8d8556423a2edccbd6', 18, 'EOS', 'EOS Token')
 // export const DOT = new Token(ChainId.MAINNET, '0x7083609fce4d1d8dc0c979aab8c869ea2c873402', 18, 'DOT', 'Polkadot Token')
@@ -67,8 +61,8 @@ const WETH_ONLY: ChainTokenList = {
 // used to construct intermediary pairs for trading
 export const BASES_TO_CHECK_TRADES_AGAINST: ChainTokenList = {
   ...WETH_ONLY,
-  [ChainId.MAINNET]: [DAI, BUSD, USDT, USDC, QUSD],
-  [ChainId.BSCTESTNET]: [tDAI, tBUSD, tUSDT, tUSDC]
+  [ChainId.MAINNET]: [DAI, BUSD, USDT, USDC, QUSD]
+//   [ChainId.BSCTESTNET]: [tDAI, tBUSD, tUSDT, tUSDC]
 }
 
 /**
@@ -84,8 +78,8 @@ export const BASES_TO_CHECK_TRADES_AGAINST: ChainTokenList = {
 // used for display in the default list when adding liquidity
 export const SUGGESTED_BASES: ChainTokenList = {
   ...WETH_ONLY,
-  [ChainId.MAINNET]: [DAI, BUSD, USDT, USDC, QUSD],
-  [ChainId.BSCTESTNET]: [tDAI, tBUSD, tUSDT, tUSDC]
+  [ChainId.MAINNET]: [DAI, BUSD, USDT, USDC, QUSD]
+//   [ChainId.BSCTESTNET]: [tDAI, tBUSD, tUSDT, tUSDC]
 }
 
 // used to construct the list of all pairs we consider by default in the frontend
--- src/pages/Pool/index.tsx
+++ src/pages/Pool/index.tsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom'
 import { SwapPoolTabs } from '../../components/NavigationTabs'
 
 import Question from '../../components/QuestionHelper'
+import QuestionHelper from '../../components/QuestionHelper'
 import FullPositionCard from '../../components/PositionCard'
 import { useUserHasLiquidityInAllTokens } from '../../data/V1'
 import { useTokenBalancesWithLoadingIndicator } from '../../state/wallet/hooks'
@@ -62,6 +63,12 @@ export default function Pool() {
     <>
       <AppBody>
         <SwapPoolTabs active={'pool'} />
+               <RowBetween padding={'8px 8px'}>
+        <Text><QuestionHelper text="There is a 0.4975% withdrawal fee that goes to STAX stakers &amp; remaining LP providers to incentivise long term liquidity. STAX holders, depending on their STAX holdings, receive discounts and even fee-free withdrawals. See more on our Medium"></QuestionHelper>
+        <span> </span> Fees | Warnings<QuestionHelper text="Our contracts have been audited, but users should always exercise caution when depositing funds. We will always act to protect our users, but by proceeding, you agree to DYOR and are responsible for any losses incurred while interacting with our product."></QuestionHelper>
+</Text>
+</RowBetween>
+        
         <AutoColumn gap="lg" justify="center">
           <ButtonPrimary id="join-pool-button" as={Link} style={{ padding: 16 }} to="/add/BUSD">
             <Text fontWeight={500} fontSize={20} style={{ color: '#fff' }}>
